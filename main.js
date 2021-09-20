485429a9d7b124929d9f8d614e7bea27adab8c49
dei27 <deivicb93@gmail.com>
2021-09-20 01:14:36 -0600

    nuevas

--- main.js
+++ main.js
@@ -28,17 +28,41 @@ app.mount("#container");
 // funcion para el saludo sincronizado
 
 (()=>{
-    const saludar = document.getElementById("titleh1");
+    const saludar = document.getElementById("titleh2");
     let fecha = new Date();
+    let day=fecha.getDay();
     let horas = fecha.getHours();
+    switch (day) {
+        case 1:
+            day="lunes"
+            break;
+        case 2:
+            day="martes"
+            break;
+        case 3:
+            day="miércoles"
+            break;
+        case 4:
+            day="jueves"
+            break;
+        case 5:
+            day="viernes"
+            break;
+        case 6:
+            day="sábado"
+            break;
+        default:
+            day="domingo"
+            break;
+    }
     if (horas<12 && horas>0) {
-        saludar.innerHTML="Buenos días"
+        saludar.innerHTML=`¡Buenos días, feliz ${day}! 🌻`
     }
     else if(horas>=12 && horas<18){
-        saludar.innerHTML="Buenas tardes"
+        saludar.innerHTML=`¡Buenas tardes, feliz ${day}! 🌼`
     }
     else{
-        saludar.innerHTML="Buenas noches"
+        saludar.innerHTML=`¡Buenas noches, feliz ${day}! 🌷`
     }
 })();
 
