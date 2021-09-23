6d08a89a44e6d8e488a7e243f7edcc97fa3a894d
dei27 <deivicb93@gmail.com>
2021-09-22 18:57:07 -0600

    Falta talleres-galeria

--- Paginas/second.js
+++ Paginas/second.js
@@ -30,47 +30,16 @@ app.mount("#container");
 // funcion para el saludo sincronizado
 
 (()=>{
-    const saludar = document.getElementById("titleh2");
-    const saludar2 = document.getElementById("titleh3");
     let fecha = new Date();
     let day=fecha.getDay();
     let horas = fecha.getHours();
-    switch (day) {
-        case 1:
-            day="lunes"
-            break;
-        case 2:
-            day="martes"
-            break;
-        case 3:
-            day="miércoles"
-            break;
-        case 4:
-            day="jueves"
-            break;
-        case 5:
-            day="viernes"
-            break;
-        case 6:
-            day="sábado"
-            break;
-        default:
-            day="domingo"
-            break;
-    }
     if (horas<12 && horas>=0) {
-        saludar.innerHTML="¡Buenos días! 🌻";
-        saludar2.innerHTML=`Feliz ${day}`;
         document.getElementById('container').classList.add('dia')
     }
     else if(horas>=12 && horas<18){
-        saludar.innerHTML="¡Buenas tardes! 🌼";
-        saludar2.innerHTML=`Feliz ${day}`;
         document.getElementById('container').classList.add('tarde')
     }
     else{
-        saludar.innerHTML="¡Buenas noches! 🌷";
-        saludar2.innerHTML=`Feliz ${day}`;
         document.getElementById('container').classList.add('noche')
     }
 })();
