3169a410806afc4cdf88160862c9c502c3ce17e0
dei27 <deivicb93@gmail.com>
2021-09-23 02:15:37 -0600

    queda talleres

--- main.js
+++ main.js
@@ -83,3 +83,8 @@ function ani(){
 /*iniciar libreria*/
 AOS.init();
 
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
